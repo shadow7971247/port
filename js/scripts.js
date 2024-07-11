e736fcef99832b94cd77e505845aff19b7d19022
shadow7971247 <jhonconstantine1996@gmail.com>
2024-07-11 19:25:34 +0500

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-// Example JavaScript code
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('Document is fully loaded');
-    // You can add more functionality here
+document.getElementById('more-button').addEventListener('click', function() {
+    alert('More information will be shown here.');
+});
+
+document.getElementById('close-button').addEventListener('click', function() {
+    alert('The window will be closed.');
 });
